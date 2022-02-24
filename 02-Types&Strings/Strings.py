4e21899030c22255c8e059e9aeb834c28c5834a4
KarolKM <karolmackowiak3@gmail.com>
2022-02-24 20:14:40 +0100

    update, after trainings

--- 02-Types&Strings/Strings.py
+++ 02-Types&Strings/Strings.py
@@ -4,11 +4,24 @@ new_txt = 't' + txt[1:]
 print(txt.replace('M', 't'))
 print(new_txt)
 print(type(txt))
+print("----------------")
 
 #ex 1:
 tekst = 'Alamakota'
-lenght=len(tekst)
-mid = lenght//2
+mid = len(tekst)//2
 print(tekst[(mid-1):(mid+2)])
+print("----------------")
 
+#ex 2 do domu!!!!
 
+print("----------------")
+
+#ex 3:
+quote = "Honesty is the first chapter in the book of wisdom."
+print('Liczba wszystkich znaków: ', len(quote)+1)
+print(quote[(-(len('wisdom')+1)):-1])
+print("----------------")
+
+#ex 4:
+
+print("----------------")
